4556ea8fdec8681be8fa67db061fb2667f1088d5
wakingrufus <wakingrufus@gmail.com>
2016-11-11 10:18:26 -0600

    more verbose validation script

--- src/dist/validate.sh
+++ src/dist/validate.sh
@@ -5,5 +5,7 @@ result=$(curl -s http://localhost:9005/elo-api/health)
 if [[ "$result" =~ "OK" ]]; then
     exit 0
 else
+    curl -vvv -s http://localhost:9005/elo-api/health
+    curl -vvv -s http://0.0.0.0:9005/elo-api/health
     exit 1
 fi
